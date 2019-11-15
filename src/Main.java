c490a3eaa15ecb8fc62e9b6bc451af66fd63f631
Oscar Wong <wong.oscar67@gmail.com>
2019-11-15 16:17:36 -0700

    final setup

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import Systems.DatabaseSystem;
+import Systems.DatabaseSystem.DatabaseSystem;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    public void start(Stage primaryStage) throws Exception{}
 
 
     public static void main(String[] args) {
