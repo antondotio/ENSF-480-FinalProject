3c0828de9acd504ed29eb308089098bcaeab44b4
JulianPinto <julianpinto@shaw.ca>
2019-11-16 13:59:19 -0700

    Enities continued

--- src/Entity/RegisteredRenterAccount.java
+++ src/Entity/RegisteredRenterAccount.java
@@ -1,6 +1,15 @@
 package Entity;
 
-public class RegisteredRenterAccount {
+import java.util.ArrayList;
+
+public class RegisteredRenterAccount extends Account {
+
     ArrayList<Notification> notification;
     ArrayList<SearchCriteria> searchCriteria;
+
+    public RegisteredRenterAccount(Name name, int accountID, String email) {
+        super(name, accountID, email);
+        notification = new ArrayList<Notification>();
+        searchCriteria = new ArrayList<SearchCriteria>();
+    }
 }
