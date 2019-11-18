19f8b060b73e424892453b720afef63237eec995
Anton Santos <43733827+antondotio@users.noreply.github.com>
2019-11-18 15:36:30 -0700

    help

--- src/Controller/ProperTeaRentalsController.java
+++ src/Controller/ProperTeaRentalsController.java
@@ -9,34 +9,35 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class ProperTeaRentalsController {
-
-    @FXML
+    @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
-    @FXML
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
-    @FXML
-    private Button loginButton;
+    @FXML // fx:id="loginButton"
+    private Button loginButton; // Value injected by FXMLLoader
 
-    @FXML
-    private PasswordField passwordForm;
+    @FXML // fx:id="passwordForm"
+    private PasswordField passwordForm; // Value injected by FXMLLoader
 
-    @FXML
-    private TextField usernameForm;
+    @FXML // fx:id="usernameForm"
+    private TextField usernameForm; // Value injected by FXMLLoader
 
 
+    // Handler for Button[fx:id="loginButton"] onAction
     @FXML
     void login(ActionEvent event) {
+        // handle the event here
     }
 
-    @FXML
+    @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert loginButton != null : "fx:id=\"loginButton\" was not injected: check your FXML file 'ProperTeaRentals.fxml'.";
         assert passwordForm != null : "fx:id=\"passwordForm\" was not injected: check your FXML file 'ProperTeaRentals.fxml'.";
         assert usernameForm != null : "fx:id=\"usernameForm\" was not injected: check your FXML file 'ProperTeaRentals.fxml'.";
 
+        // Initialize your logic here: all @FXML variables will have been injected
 
     }
-
 }
