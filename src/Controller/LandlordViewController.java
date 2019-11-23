c4efc71902bb459ceeb7005e64b84fa77070eb31
Anton Santos <43733827+antondotio@users.noreply.github.com>
2019-11-22 18:10:37 -0700

    bruh

--- src/Controller/LandlordViewController.java
+++ src/Controller/LandlordViewController.java
@@ -1,4 +1,38 @@
 package Controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleGroup;
+
+
 public class LandlordViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ToggleGroup furnished;
+
+    @FXML
+    private Button logoutButton;
+
+
+    @FXML
+    void logout(ActionEvent event) {
+    }
+
+    @FXML
+    void initialize() {
+        assert furnished != null : "fx:id=\"furnished\" was not injected: check your FXML file 'LandlordView.fxml'.";
+        assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'LandlordView.fxml'.";
+
+
+    }
+
 }
