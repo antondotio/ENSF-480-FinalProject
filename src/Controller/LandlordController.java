ef67750ab66182f8a19cdc5fe3c8b0b7c1a72fcf
Oscar Wong <wong.oscar67@gmail.com>
2019-11-23 23:12:53 -0700

    note

--- src/Controller/LandlordController.java
+++ src/Controller/LandlordController.java
@@ -16,6 +16,9 @@ public class LandlordController {
         if (newState.equals("Active")) {
             return false;   //  landlord cannot change state to active, only managers can
         }
+        // TODO: there's a bug, the above fires even if previous state is say, rented
+        //  landlords shouldn't be able to go suspended->active, but they should be able to go from rented to active...
+
         return listingController.updateListingState(listingId, newState);
     }
 
